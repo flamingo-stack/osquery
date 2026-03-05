113e48eef8000144f76c50c940154fb66d87e278
kirill-567 <kirill@flamingo.cx>
2026-03-05 22:28:02 +0100

    Sanitize OpenFrame logs

--- plugins/remote/enroll/tls_enroll.cpp
+++ plugins/remote/enroll/tls_enroll.cpp
@@ -76,7 +76,6 @@ std::string TLSEnrollPlugin::enroll() {
   
   // Add the prefix "/tools/agent/fleetmdm-server" to all requests only if openframe mode is enabled
   if (FLAGS_openframe_mode) {
-    LOG(INFO) << "Adding /tools/agent/fleetmdm-server to URI for openframe mode";
     uri += "/tools/agent/fleetmdm-server";
   }
   
